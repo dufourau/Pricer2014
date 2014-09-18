e86936a653499c162041fdbca171fdabf7028495
Romain Pouzaud <romain.pouzaud@ensimag.grenoble-inp.fr>
2014-09-18 11:57:12 +0200

    Add option barrier low constructor

--- src/option.cpp
+++ src/option.cpp
@@ -5,9 +5,9 @@ using namespace std;
 #include "option.h"
 
 Option::Option(double T_, int timeSteps_, int size_){
-  this.T_ = T_;
-  this.timeSteps_ = timeSteps_;
-  this.size_ = size_;
+  this->T_ = T_;
+  this->TimeSteps_ = timeSteps_;
+  this->size_ = size_;
 }
 
 Option::~Option(){}
