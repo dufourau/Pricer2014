20df2ad101c2f437c0c64a8cf0c2e7aa351f9d6e
Aurélien Dufour <Aurlien.Dufour@ensimag.grenoble-inp.fr>
2014-09-23 19:00:14 +0200

    Final debug of delta, now return correct values

--- src/mc.cpp
+++ src/mc.cpp
@@ -192,7 +192,8 @@ void MonteCarlo::price(const PnlMat *past, double t, double &prix, double &ic){
 void MonteCarlo::delta(const PnlMat *past, double t, PnlVect *delta, PnlVect *ic){
 
   int nbAsset = this->opt_->size_;
-
+  PnlMat* path_shift_up = pnl_mat_create(this->opt_->TimeSteps_+1, nbAsset);
+  PnlMat* path_shift_down = pnl_mat_create(this->opt_->TimeSteps_+1, nbAsset);
   for (int i = 0; i < nbAsset; ++i)
   {
     double sum = 0;
@@ -201,22 +202,28 @@ void MonteCarlo::delta(const PnlMat *past, double t, PnlVect *delta, PnlVect *ic
     for (int j = 0; j < this->samples_; ++j)
     {
 
-      this->mod_->asset(path, t, this->opt_->TimeSteps_, this->opt_->T_, this->rng, past);
-      //this->mod_->asset(path, opt_->T_, opt_->TimeSteps_, this->rng);
-      PnlMat* path_shift_up = pnl_mat_create(this->opt_->TimeSteps_+1, nbAsset);
-      PnlMat* path_shift_down = pnl_mat_create(this->opt_->TimeSteps_+1, nbAsset);
+      //Select the right asset method to call
+      if(t==0){
+        this->mod_->asset(path, opt_->T_, opt_->TimeSteps_, this->rng);
+      }else{
+        this->mod_->asset(path, t, this->opt_->TimeSteps_, this->opt_->T_, this->rng, past);
+      }
+      
       this->mod_->shift_asset(path_shift_up, path, i, this->h_, t, this->opt_->TimeSteps_);
       this->mod_->shift_asset(path_shift_down, path, i, -this->h_, t, this->opt_->TimeSteps_);
       pnl_mat_eq(path_shift_up, path_shift_down);
 
-      // cout << "\n=\n=\n=Path shift up \n\n\n";
-      // pnl_mat_print(path_shift_up);
-      // cout << "\n=\n=\n=Path shift down \n\n\n";
-      // pnl_mat_print(path_shift_down);
-      
+  
       sum += this->opt_->payoff(path_shift_up) - this->opt_->payoff(path_shift_down);
       //cout << "Sum = " << sum << endl;
     }
-    LET(delta, i) = sum * exp(this->mod_->r_ * (this->opt_->T_ - t) / (2 * this->samples_ * MGET(past, past->m-1, i) * this->h_));
+
+    if(t==0){
+      LET(delta, i) = sum * exp(-this->mod_->r_ * (this->opt_->T_ - t)) / (2 * this->samples_ * MGET(path, 0, i) * this->h_);
+    }else{
+      LET(delta, i) = sum * exp(-this->mod_->r_ * (this->opt_->T_ - t)) / (2 * this->samples_ * MGET(past, past->m-1, i) * this->h_);
+    }
   }
+  pnl_mat_free(&path_shift_up);
+  pnl_mat_free(&path_shift_down);
 }
--- src/test_bs.cpp
+++ src/test_bs.cpp
@@ -14,43 +14,40 @@ int main(int argc, char **argv)
   int size;
   char *infile = argv[1];
   Param *P = new Parser(infile);
- 
-  
-  
   P->extract("maturity", T);
   P->extract("option size", size);
   P->extract("spot", spot, size);
   P->extract("volatility", sigma, size);
   P->extract("interest rate", r);
   P->extract("correlation",rho);
-  
-  //TEST gaussien vector
   //Initiliaze the generator
-  
   PnlRng *rng;
   rng= pnl_rng_create (PNL_RNG_MERSENNE);
   pnl_rng_sseed (rng, 0);
   PnlMat *path;
-  path= pnl_mat_create(7,size);
+  PnlVect *delta;
   PnlMat *past_path;
-  //Test mc
   MonteCarlo *mc;
   mc= new MonteCarlo(P);
   double prix;
   double ic;
 
-  mc->mod_->asset(path,1,6,rng);
+
+  path= pnl_mat_create(2,size);
+  mc->mod_->asset(path,3,1,rng);
   cout<<"path avant"<<endl; 
+  pnl_mat_print(path);
   //Copy the result
   past_path= pnl_mat_copy(path);
   pnl_mat_free(&path);
-  pnl_mat_print(past_path);
-  PnlVect *delta;
+  
   delta= pnl_vect_create(size);
-  
-  mc->delta(past_path,0.5, delta, NULL);
+  //for barrier_u
+  //mc->delta(past_path,0.5, delta, NULL);
+  //for basket_1
+  mc->delta(past_path,0,delta,NULL);
+  cout<<"Le vecteur delta: "<<endl;
   pnl_vect_print(delta);
-  
   pnl_vect_free(&delta);
   pnl_mat_free(&path);
   pnl_mat_free(&past_path);
